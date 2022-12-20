7ba41595bd58f640740fb9fbfb28c277b4e8f4cc
Ricardo <ricardo.sousa13@hotmail.com>
2022-12-20 16:25:06 +0000

    updated

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,7 +7,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state= {
-        posts:[]
+        posts:[],
+        selectedPostID : null
     }
     componentDidMount(){
        axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -23,19 +24,25 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostID: id});
+    }
+   
+
     render () {
         const posts = this.state.posts.map(post =>{
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post key={post.id} title={post.title} author={post.author} view={() =>this.postSelectedHandler(post.id)} />
         }
-
         );
+
+
         return (
             <div>
                 <section className="Posts">
                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostID} />
                 </section>
                 <section>
                     <NewPost />
